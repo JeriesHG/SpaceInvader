539e8d1ae9942c5d3b3ad247356bb7c11824e542
Jeries Handal <JeriesHG@Gmail.com>
2014-09-04 18:56:42 -0600

    refactored touchinputimages

--- Assets/Scripts/Player/PlayerTouchControl.cs
+++ Assets/Scripts/Player/PlayerTouchControl.cs
@@ -3,27 +3,35 @@ using System.Collections;
 
 public class PlayerTouchControl : MonoBehaviour {
 
-
-	public Rect positionTop = new Rect (100f, 350f,70f,80f);
-	public Rect positionBottom = new Rect (100f, 500f,70f,80f);
-	public Rect positionLeft = new Rect (120f, 200f,70f,80f);
-	public Rect positionRight = new Rect (100f, 350f,70f,80f);
+	[HideInInspector]
+	public Rect positionTop;
+	[HideInInspector]
+	public Rect positionBottom;
+	[HideInInspector]
+	public Rect positionLeft;
+	[HideInInspector]
+	public Rect positionRight;
 
 	[HideInInspector]
 	public Color color = new Color(92,92,92,0.5f);
 	
 	void OnGUI (){  
-		DrawRectangle (positionTop, color); 
-		DrawRectangle (positionBottom, color); 
-		DrawRectangle (positionRight, color);   
-		DrawRectangle (positionLeft, color);   
+		positionTop = new Rect (75f, (Screen.height-180f),50f,50f);
+		positionBottom = new Rect (75f, (Screen.height-70f),50f,50f);
+		positionLeft = new Rect (20f, (Screen.height-125f),50f,50f);
+		positionRight = new Rect (130f, (Screen.height-125f),50f,50f);
+		DrawRectangle (positionTop, color,"touchUp"); 
+		DrawRectangle (positionBottom, color,"touchDown"); 
+		DrawRectangle (positionRight, color,"touchRight");   
+		DrawRectangle (positionLeft, color,"touchLeft");   
 	}
 	
-	void DrawRectangle (Rect position, Color color)
+	void DrawRectangle (Rect position, Color color,string name)
 	{    
 		Texture2D texture = new Texture2D(1, 1);
 		texture.SetPixel(0,0,color);
 		texture.Apply();
+		texture.name = name;
 		GUI.skin.box.normal.background = texture;
 		GUI.Box(position, GUIContent.none);
 	}
